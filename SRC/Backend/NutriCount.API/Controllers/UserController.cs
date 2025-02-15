5bbba47a507def0f71f1a589fefb05c7ac0405ad
gabyfachini <gabryellafachini@outlook.com>
2025-02-15 16:21:31 -0300

    Implement user API endpoints

---
+++ SRC/Backend/NutriCount.API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NutriCount.Communication.Request;
+using NutriCount.Communication.Responses;
+
+namespace NutriCount.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost] //o tipo do endpoint
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        public IActionResult Regiter(RequestRegisterUserJson request) //função do endpoint
+        {
+            return Created();
+        }
+    }
+}
---
+++ SRC/Shared/NutriCount.Communication/Request/RequestRegisterUserJson.cs
@@ -0,0 +1,10 @@
+﻿namespace NutriCount.Communication.Request
+{
+    public class RequestRegisterUserJson
+    {
+        public string Name { get; set; } = string.Empty; //o valor inicial da propriedade será uma string vazia
+        public string Email { get; set; } = string.Empty; //o valor inicial da propriedade será uma string vazia
+        public string Password { get; set; } = string.Empty; //o valor inicial da propriedade será uma string vazia
+
+    }
+}

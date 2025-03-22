d09f469fb9f20f1172aa19c49107bea573009819
gabyfachini <gabryellafachini@outlook.com>
2025-03-22 08:41:18 -0300

    Unit Tests Implementation

---
+++ Tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,16 @@
+﻿using NutriCount.Application.UseCases.User.Register;
+
+namespace Validators.Test.User.Register
+{
+    public class RegisterUserValidatorTest
+    {
+        [Fact]
+        public void Sucess()
+        {
+            var validator = new RegisterUserValidator();
+            var result = validator.Validate(request);
+            //result.IsValid == true
+
+        }
+    }
+}

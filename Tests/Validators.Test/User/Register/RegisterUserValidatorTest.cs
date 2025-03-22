356698b88c4b7e9aa0d41bd4fa44f3790c5939eb
gabyfachini <gabryellafachini@outlook.com>
2025-03-22 19:04:41 -0300

    Test Implemetation

--- Tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
+++ Tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -1,15 +1,20 @@
-﻿using NutriCount.Application.UseCases.User.Register;
+﻿using CommonTestUtilities.Requests;
+using NutriCount.Application.UseCases.User.Register;
 
 namespace Validators.Test.User.Register
 {
     public class RegisterUserValidatorTest
-    {
+    { 
         [Fact]
         public void Sucess()
         {
             var validator = new RegisterUserValidator();
+
+            var request = RequestRegisterUserJsonBuilder.Build();
+
             var result = validator.Validate(request);
-            //result.IsValid == true
+            
+            /*result.IsValid.Should().BeTrue();*/
 
         }
     }
